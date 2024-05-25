bd3dfa888abb8238f3fdd35c491f8dd1d36ef076
aditya <aditya.sen1hl@gmail.com>
2024-05-25 23:08:20 +0430

    try vercel

--- DJ/urls.py
+++ DJ/urls.py
@@ -17,13 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-import static
-from DJ import settings
 
 urlpatterns = [
-    path("", include("main.urls")),
+    path('', include('main.urls')),
     path("admin/", admin.site.urls),
     # path('register',include('main.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
