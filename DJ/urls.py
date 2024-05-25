5987ad3d58609d4e5125dc6f2991b15f98369391
aditya <aditya.sen1hl@gmail.com>
2024-05-25 23:01:31 +0430

    try vercel

--- DJ/urls.py
+++ DJ/urls.py
@@ -19,7 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('main.urls')),
+    path("", include("main.urls")),
     path("admin/", admin.site.urls),
     # path('register',include('main.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- build_files.sh
+++ build_files.sh
@@ -3,3 +3,28 @@ python3 --version
 python3 -m ensurepip --upgrade
 pip3 install -r requirements.txt
 # python3 manage.py collectstatic --noinput
+
+
+#!/bin/bash
+
+# Update pip
+echo "Updating pip..."
+python3 pip install -U pip
+
+# Install dependencies
+
+echo "Installing project dependencies..."
+python3 -m pip install -r requirements.txt
+
+# Make migrations
+echo "Making migrations..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+#Install whitenoise
+echo "Installing white noise..."
+python3 manage.py whitenoise
+# Collect staticfiles
+echo "Collect static..."
+python3 manage.py collectstatic --noinput --clear
+
+echo "Build process completed!"```
